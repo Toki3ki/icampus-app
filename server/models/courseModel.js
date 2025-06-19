8875227bf314946fb9120c915ccf03abfb70b8fe
Lixinran1826 <lixinran1826@163.com>
2025-06-19 10:34:05 +0800

    edit courseModel

--- server/models/courseModel.js
+++ server/models/courseModel.js
@@ -23,22 +23,22 @@ const courseSchema = new mongoose.Schema({
   ],
   schedule: [ // schedule is an array of objects defining the course schedule
     {
-      dayOfWeek: { // 表示星期几，例如：1 (周一) - 7 (周日)
+      dayOfWeek: { // day of the week, 1 for Monday, 2 for Tuesday, ..., 7 for Sunday
         type: Number,
         required: true,
         min: 1,
         max: 7,
       },
-      startTime: { // 开始时间，建议使用字符串 'HH:MM' 或 Date 类型
+      startTime: {// satart time of the class
         type: String,
         required: true,
-        // 可以添加正则验证，确保格式为 HH:MM
-        // match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
+        // regular expression to match time format HH:MM
+        match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
       },
       endTime: { // 结束时间
         type: String,
         required: true,
-        // match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
+        match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
       },
     }
   ],
